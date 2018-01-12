34e3b76bf615756272a9fcc3eccc2bf48286bda6
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-01-12 14:49:40 -0600

    Add function implementation and tests

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-function repeat () {
-
+function repeat (stringToRepeat, timesToRepeat) {
+  let s = '';
+  for (let i = 0; i < timesToRepeat; i++) {
+    s += stringToRepeat;
+  }
+  return s;
 }
 
-function runningSum () {
-
+function runningSum (array) {
+  let a = [];
+  for (let i = 0; i < array.length; i++) {
+    let prev = 0;
+    if (i > 0) {
+      prev = a[i - 1];
+    }
+    a[i] = array[i] + prev;
+  }
+  return a;
 }
 
-function slice () {
+function slice (object, properties) {
+  let o = {};
+  properties.forEach(function (property) {
+    if (object[property]) {
+      o[property] = object[property];
+    }
+  });
+  return o;
 }
 
-export default {
-  repeat,
-  runningSum,
-  slice
+module.exports = {
+  repeat: repeat,
+  runningSum: runningSum,
+  slice: slice
 };
